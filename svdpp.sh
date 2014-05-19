1713a8d082cbd30ec8ecdb945d0a0567b75f57a2
Aleksander Bello <belloaleksander@gmail.com>
2014-05-19 02:00:34 -0700

    Using variable instead of hardcoded path

--- svdpp.sh
+++ svdpp.sh
@@ -4,57 +4,57 @@ UMR="data/umr-noprobe.dta"
 PRED="data/qualprobe.pred"
 
 # Template
-# ./toolkits/collaborative_filtering/svdpp --training=data/umr.dta \
+# ./toolkits/collaborative_filtering/svdpp --training=$UMR \
 # --svdpp_item_bias_step=0.003 --svdpp_user_bias_step=0.012 --svdpp_user_factor_step=0.006 \
 # --svdpp_user_factor2_step=0.006 --svdpp_item_bias_reg=0.06 --svdpp_user_bias_reg=0.060 \
 # --svdpp_user_factor_reg=0.160 --svdpp_user_factor2_reg=0.160  \
 # --minval=1 --maxval=5 --max_iter=15 --quiet=1 \
-# --test=data/qual.pred --D=20
+# --test=$PRED --D=20
 
 
 # 0.49% above water
-# ./toolkits/collaborative_filtering/svdpp --training=data/umr.dta \
+# ./toolkits/collaborative_filtering/svdpp --training=$UMR \
 # --svdpp_item_bias_step=0.003 --svdpp_user_bias_step=0.012 --svdpp_user_factor_step=0.006 \
 # --svdpp_user_factor2_step=0.006 --svdpp_item_bias_reg=0.03 --svdpp_user_bias_reg=0.020 \
 # --svdpp_user_factor_reg=0.010 --svdpp_user_factor2_reg=0.010  \
 # --minval=1 --maxval=5 --max_iter=15 --quiet=1 \
-# --test=data/qual.pred --D=20
+# --test=$PRED --D=20
 
 # 1.08 above water
-# ./toolkits/collaborative_filtering/svdpp --training=data/umr.dta \
+# ./toolkits/collaborative_filtering/svdpp --training=$UMR \
 # --svdpp_item_bias_step=0.005 --svdpp_user_bias_step=0.018 --svdpp_user_factor_step=0.009 \
 # --svdpp_user_factor2_step=0.009 --svdpp_item_bias_reg=0.06 --svdpp_user_bias_reg=0.060 \
 # --svdpp_user_factor_reg=0.160 --svdpp_user_factor2_reg=0.160  \
 # --minval=1 --maxval=5 --max_iter=15 --quiet=1 \
-# --test=data/qual.pred --D=20
+# --test=$PRED --D=20
 
 
 
 # 0.56 above water
-# ./toolkits/collaborative_filtering/svdpp --training=data/umr.dta \
+# ./toolkits/collaborative_filtering/svdpp --training=$UMR \
 # --svdpp_item_bias_step=0.005 --svdpp_user_bias_step=0.018 --svdpp_user_factor_step=0.009 \
 # --svdpp_user_factor2_step=0.009 --svdpp_item_bias_reg=0.06 --svdpp_user_bias_reg=0.060 \
 # --svdpp_user_factor_reg=0.160 --svdpp_user_factor2_reg=0.160  \
 # --minval=1 --maxval=5 --max_iter=15 --quiet=1 \
-# --test=data/qual.pred --D=25
+# --test=$PRED --D=25
 
 
 # 1.31 above water
-# ./toolkits/collaborative_filtering/svdpp --training=data/umr.dta \
+# ./toolkits/collaborative_filtering/svdpp --training=$UMR \
 # --svdpp_item_bias_step=0.005 --svdpp_user_bias_step=0.018 --svdpp_user_factor_step=0.009 \
 # --svdpp_user_factor2_step=0.009 --svdpp_item_bias_reg=0.06 --svdpp_user_bias_reg=0.060 \
 # --svdpp_user_factor_reg=0.160 --svdpp_user_factor2_reg=0.160  \
 # --minval=1 --maxval=5 --max_iter=25 --quiet=1 \
-# --test=data/qual.pred --D=20
+# --test=$PRED --D=20
 
 
 # 0.71 above water 
-# ./toolkits/collaborative_filtering/svdpp --training=data/umr.dta \
+# ./toolkits/collaborative_filtering/svdpp --training=$UMR \
 # --svdpp_item_bias_step=0.005 --svdpp_user_bias_step=0.018 --svdpp_user_factor_step=0.009 \
 # --svdpp_user_factor2_step=0.009 --svdpp_item_bias_reg=0.06 --svdpp_user_bias_reg=0.060 \
 # --svdpp_user_factor_reg=0.160 --svdpp_user_factor2_reg=0.160  \
 # --minval=1 --maxval=5 --max_iter=25 --quiet=1 \
-# --test=data/qual.pred --D=25
+# --test=$PRED --D=25
 
 
 ./toolkits/collaborative_filtering/svdpp --training=$UMR \
