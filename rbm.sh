7213727b33d49f47686d9bc6af8de764b6037823
Aleksander Bello <belloaleksander@gmail.com>
2014-05-21 13:03:05 -0700

    Latest rbm stuff

--- rbm.sh
+++ rbm.sh
@@ -16,15 +16,48 @@ VAL="data/probe-validation"
 # --max_iter=15 --quiet=1 --test=$PRED
 
 
-# 
+# 0.77
 # ./toolkits/collaborative_filtering/rbm --training=$UMR --minval=1 --maxval=5  \
 # --rbm_mult_step_dec=1 --rbm_alpha=0.005 --rbm_beta=0.006 --rbm_bins=6 \
 # --max_iter=15 --quiet=1 --test=$PRED
 
 
+# 1.81
+# ./toolkits/collaborative_filtering/rbm --training=$UMR --validation=$VAL --minval=1 --maxval=5  \
+# --rbm_mult_step_dec=1 --rbm_alpha=0.005 --rbm_beta=0.003 --rbm_bins=6 \
+# --max_iter=15 --quiet=1 --test=$PRED \
+# --D=40 \
+# --halt_on_rmse_increase=0
+
+# 2.40%
+# ./toolkits/collaborative_filtering/rbm --training=$UMR --validation=$VAL --minval=1 --maxval=5  \
+# --rbm_mult_step_dec=0.999 --rbm_alpha=0.005 --rbm_beta=0.005 --rbm_bins=6 \
+# --max_iter=30 --quiet=1 --test=$PRED \
+# --D=200 \
+# --halt_on_rmse_increase=15
+
+
+# 2.35
+# ./toolkits/collaborative_filtering/rbm --training=$UMR --validation=$VAL --minval=1 --maxval=5  \
+# --rbm_mult_step_dec=0.999 --rbm_alpha=0.005 --rbm_beta=0.008 --rbm_bins=6 \
+# --max_iter=30 --quiet=1 --test=$PRED \
+# --D=200 \
+# # --halt_on_rmse_increase=22
+
+
+# 349
+# ./toolkits/collaborative_filtering/rbm --training=$UMR --validation=$VAL --minval=1 --maxval=5  \
+# --rbm_mult_step_dec=0.999 --rbm_alpha=0.001 --rbm_beta=0.002 --rbm_bins=6 \
+# --max_iter=50 --quiet=1 --test=$PRED \
+# --D=200 \
+# # --halt_on_rmse_increase=22
+
+
 ./toolkits/collaborative_filtering/rbm --training=$UMR --validation=$VAL --minval=1 --maxval=5  \
---rbm_mult_step_dec=1 --rbm_alpha=0.005 --rbm_beta=0.003 --rbm_bins=6 \
---max_iter=15 --quiet=1 --test=$PRED
+--rbm_mult_step_dec=0.999 --rbm_alpha=0.001 --rbm_beta=0.002 --rbm_bins=6 \
+--max_iter=100 --quiet=1 --test=$PRED \
+--D=200 \
+# --halt_on_rmse_increase=22
 
 cd data && python parse.py
 
